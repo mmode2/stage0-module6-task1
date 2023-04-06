3c5c55d8a28d802d8fea8ff30b0eee8057530e5c
Maiko <maiko.mode05@gmail.com>
2023-04-06 13:37:24 +0400

    done

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    private int wings;
 
+    public Bird() {
+        super("blue", 2, false);
+        this.wings = 2;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + String.format(" Moreover, it has %d wings and can fly.", this.wings);
+    }
 }
